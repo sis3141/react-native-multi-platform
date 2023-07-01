2235672219fac77f5fe7171c42b4d98b0d8a79be
KimJinsu <sis3141@unist.ac.kr>
2023-07-01 16:40:04 +0900

    pm2 & nodemone web serve setting added not ignore .vscode folder ( my preference )

---
+++ packages/server/main.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const express = require('express');
+const  path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const port = 3000;
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../web/build')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/*', (req, res) => {
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    Pragma: 'no-cache',
+    Date: Date.now(),
+  });
+  res.sendFile(path.join(__dirname, '../web/build', 'index.html'));
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
---
+++ packages/server/pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name : 'athler',
+    script : './main.js',
+    watch : ['./main.js', '../web/build/index.html'],
+    interpreter  :'ts-node',
+    "interpreter_args" : '-r tsconfig-paths/register'
+  }]
+}
