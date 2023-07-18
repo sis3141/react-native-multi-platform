537d4680e42ab5644a4dc5a1b95338e235852a9c
KimJinsu <sis3141@unist.ac.kr>
2023-07-18 19:34:56 +0900

    web build configure - craco config

--- packages/web/craco.config.js
+++ packages/web/craco.config.js
@@ -1,34 +1,48 @@
 const webpack = require("webpack");
 const { getWebpackTools } = require("react-native-monorepo-tools");
-const path = require('path');
+const path = require("path");
 
 const monorepoWebpackTools = getWebpackTools();
 
 module.exports = {
+  babel: {
+    plugins: ["babel-plugin-react-native-web"],
+    presets: ["@babel/preset-react"],
+  },
   webpack: {
+    configure: {
+      module: {
+        rules: [
+          {
+            test: /(\.ts$|\.tsx$)/,
+            use: "babel-loader",
+          },
+        ],
+      },
+    },
     configure: (webpackConfig) => {
       // Allow importing from external workspaces.
-      monorepoWebpackTools.enableWorkspacesResolution(webpackConfig);
+      // monorepoWebpackTools.enableWorkspacesResolution(webpackConfig);
       // Ensure nohoisted libraries are resolved from this workspace.
       monorepoWebpackTools.addNohoistAliases(webpackConfig);
 
       // add the top level package as a app source path so our nohoisted
       const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
-          ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+        ({ constructor }) =>
+          constructor && constructor.name === "ModuleScopePlugin"
       );
-
-      const [ clientSrc ] = webpackConfig.resolve.plugins[scopePluginIndex].appSrcs;
-      const psvServices = path.resolve(clientSrc,'../..');
-
+      const [clientSrc] =
+        webpackConfig.resolve.plugins[scopePluginIndex].appSrcs;
+      const psvServices = path.resolve(clientSrc, "../..");
       webpackConfig.resolve.plugins[scopePluginIndex].appSrcs.push(psvServices);
-
+      // console.log("webpackconfig : \n", JSON.stringify(webpackConfig));
       return webpackConfig;
     },
     plugins: [
       // Inject the "__DEV__" global variable.
       new webpack.DefinePlugin({
         __DEV__: process.env.NODE_ENV !== "production",
-      })
+      }),
     ],
   },
 };
